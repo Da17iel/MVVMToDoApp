204424433207b4c84cedd881ff53e2703333cbb2
Daniel Greil <danimickigreil@gmail.com>
2023-11-15 09:27:09 +0100

    Improved ShowAndCreateViewModel and edit classdiagram

--- ViewModels/ShowAndCreateViewModel.cs
+++ ViewModels/ShowAndCreateViewModel.cs
@@ -1,13 +1,5 @@
 ﻿using MVVMToDoApp.Commands;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
 
 namespace MVVMToDoApp.ViewModels
@@ -82,12 +74,6 @@ namespace MVVMToDoApp.ViewModels
         {
             Tasks = new ObservableCollection<Models.Task>();
             AddTask = new AddTaskCommand(this);
-
-            Tasks.Add(new Models.Task("New Task", "Desc.", Models.TaskStatus.NOTDONE));
-            Tasks.Add(new Models.Task("TestTask 1", "Sample Text 1", Models.TaskStatus.DUE));
-            Tasks.Add(new Models.Task("TestTask 2", "Sample Text 2", Models.TaskStatus.INPROGRESS));
-            Tasks.Add(new Models.Task("TestTask 3", "Sample Text 3", Models.TaskStatus.NOTDONE));
-
         }
     }
 }
